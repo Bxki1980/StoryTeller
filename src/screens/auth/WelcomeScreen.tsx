3c52452b46f23f9a84286bbcb54155a9e3a4b5c4
Mohammad Fard <mohammadahmadifard0@gmail.com>
2025-06-02 18:29:08 -0400

    refactor(WelcomeScreen): replace Button with TouchableOpacity for improved styling and consistency

--- src/screens/auth/WelcomeScreen.tsx
+++ src/screens/auth/WelcomeScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Text, Image, View, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import type { ParamListBase } from '@react-navigation/native';
@@ -25,9 +24,11 @@ export default function WelcomeScreen() {
           source={require('../../assets/images/auth/js.jpeg')}
           className="mt-10 h-1/2 w-full"
           resizeMode="contain"></Image>
-        <View className="mt-10 h-12 w-1/2 items-center justify-center rounded-full bg-blue-500">
-          <Button title="Get Started!" onPress={() => {}}></Button>
-        </View>
+        <TouchableOpacity
+          onPress={() => {}}
+          className="mt-10 h-12 w-1/2 items-center justify-center rounded-full bg-blue-500 shadow-md">
+          <Text className="text-base font-bold uppercase text-white">Get Started!</Text>
+        </TouchableOpacity>
         <View className="mt-5 flex-row">
           <Text className="text-gray-500">Already have an account?</Text>
           <TouchableOpacity onPress={() => navigation.navigate('auth')}>
